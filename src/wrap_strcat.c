c75e8a3794fda695d9fc60238a1ceb3a3e0aca6d
jalcim <alcim.dev@gmail.com>
2019-06-08 01:36:34 +0200

    Update wrap_strcat.c

--- src/wrap_strcat.c
+++ src/wrap_strcat.c
@@ -4,9 +4,5 @@
 
 void *wrap_strcat(void **arg)
 {
-  char *str1, *str2;
-
-  str1 = (char *)arg[0];
-  str2 = (char *)arg[1];
-  return (strcat(str1, str2));
+  return (strcat((char *)arg[1], (char *)arg[0]));
 }
